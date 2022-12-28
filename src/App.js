ec445ba40cabc61f30fa4348776fb113ebb4bdff
hateriam <anohov_1983@mail.ru>
2022-12-28 20:24:07 +0300

    add cross and edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+
 import React, { useState } from 'react'
 
 
 function App() {
   const [notes, setNotes] = useState([])
   const [value, setValue] = useState('Введите задание')
+  const [isEdit, setIsEdit] = useState(false)
   
   let result = notes.map((note, index) => {
+    if (!isEdit) {
+      note = notes[index]
+    } else {
+      note = <input className='editinput' value={notes[index]} onChange={event => changeData(index, event)} />
+    }
     return <ol key={index}>
         {note}
-        <button>V</button>
-        <button onClick={() => delTask(index)}>X</button>
+        <button className='notebutton' onClick={event => itDone(event)}>V</button>
+        <button className='notebutton' onClick={() => setIsEdit(!isEdit)}> {isEdit ? 'Сохр.' : 'Редакт.'}</button>
+        <button className='notebutton' onClick={() => delTask(index)}>X</button>
     </ol> 
-  })
-  
-  function delTask(index) {
-    setNotes([...notes.slice(0, index), ...notes.slice(index + 1)])
+    })
+    
+    function changeData(index, event) {
+      setNotes([...notes.slice(0, index), event.target.value, ...notes.slice(index + 1)])
+      }
+    
+    
+    function itDone(event) {
+      let elem = event.target
+      elem.closest('ol').classList.toggle('crossline')
     }
     
-    
+    function delTask(index) {
+      setNotes([...notes.slice(0, index), ...notes.slice(index + 1)])
+      }
+
     function add () {
       setNotes([...notes, value])
     }
@@ -30,3 +47,5 @@ function App() {
   }
 
 export default App;
+
+
